235032955e9e7ed6976c4fa5692361a96dc55031
Korhan Mutludoğan <korhanmd@gmail.com>
2018-08-30 22:28:11 +0300

    Add gameboard and user classes

---
+++ mainClasses.cpp
@@ -0,0 +1,19 @@
+class Gameboard{
+	char gameSpace[4][4];
+public:
+	Gameboard();
+	void setGameSpace(int row, int column, char value);
+	char getGameSpace(int row, int column);
+	int checkResult();
+	int printBoard();
+};
+
+class User{
+	string name;
+	char value;
+public:
+	void setName(string nameIn);
+	void setValue(char valueIn);
+	string getName();
+	char getValue();
+}
