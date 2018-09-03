4065cd41df7048bd5fb2a0722e7fe6ca21741249
Korhan Mutludoğan <korhanmd@gmail.com>
2018-09-03 23:17:44 +0300

    Add loop for players' turns

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(){
 	User userX, userO;
 	string nameX, nameO;
 	Gameboard board;
+	int row, column;
 
 	userX.setValue('x');
 	userO.setValue('o');
@@ -22,6 +23,25 @@ int main(){
 	cout << "O player: " << userO.getName() << endl;
 
 	board.printBoard();
+
+	for (int i = 0; i < 16; i++){
+		if (i % 2 == 0){
+			cout << userX.getName() << "'s turn. Enter row: ";
+			cin >> row;
+			cout << "Enter column: ";
+			cin >> column;
+			board.setGameSpace(row, column, userX.getValue());
+		}
+		else if (i % 2 == 1){
+			cout << userO.getName() << "'s turn. Enter row: ";
+			cin >> row;
+			cout << "Enter column: ";
+			cin >> column;
+			board.setGameSpace(row, column, userO.getValue());
+		}
+
+		board.printBoard();
+	}
 
 	return 0;
 }
